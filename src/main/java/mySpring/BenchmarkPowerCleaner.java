4105acba9025dfdbe5c1d8829651df52e42e332c
bsevgeny@gmail.com <papakita2009>
2017-11-07 11:16:05 +0200

    proxy / decorator design pattern

---
+++ src/main/java/mySpring/BenchmarkPowerCleaner.java
@@ -0,0 +1,14 @@
+package mySpring;
+
+public class BenchmarkPowerCleaner implements Cleaner {
+    @InjectByType
+    private PowerCleaner powerCleaner;
+
+    @Override
+    public void clean() {
+        long start = System.nanoTime();
+        powerCleaner.clean();
+        long end = System.nanoTime();
+        System.out.println(end-start);
+    }
+}
